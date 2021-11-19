a1d8e4d50a7573abc41a0dfe330dc0a4c8d6837f
Umid <u.hikmatov@oson.uz>
2021-11-19 12:05:31 +0300

    Initial commit

---
+++ src/test/java/com/umid/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umid.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
